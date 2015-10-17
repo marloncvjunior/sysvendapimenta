0f3a496785f2700bcec84bb07aa104bc9fcabfc8
Wilton Silva <wilton.tecti@gmail.com>
2015-10-16 21:24:58 -0300

    add layout.php

---
+++ layout.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/layout.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap/js/bootstrap.js" type="text/javascript"></script>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top navbar-inverse">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bar1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Menu</a>
+            </div>
+            <div class="container"
+                 <div class="collapse navbar-collapse" id="bar1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
+                        <li><a href="#">Link 3</a></li>
+                </div>
+                </ul>
+            </div>
+
+        <!-- Código acima cria a barra fixa do site -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">Paranawe</div>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            </div>
+        </div>
+
+
+
+    </body>
+</html>
